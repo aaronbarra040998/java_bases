fb88b28a468c89ba0d018ab87a37c69bfda7deb1
Aaron A. Barra <121281535+aaronbarra040998@users.noreply.github.com>
2024-04-09 22:40:25 -0500

    primera parte

---
+++ src/temperatura.java
@@ -0,0 +1,11 @@
+public class temperatura {
+    public static void main(String[] args) {
+        System.out.println("convertidor de temperatura");
+        double temperatura = 34.12;
+        System.out.println("se ingreso"+ temperatura);
+        double cambio;
+        cambio = (temperatura*1.8)+32;
+
+        System.out.println("la temperatura es:"+cambio+"grados Farenheid");
+    }
+}
